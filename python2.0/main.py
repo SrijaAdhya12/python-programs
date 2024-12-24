afdc1ad19362b4f07e8a3f49e26ef8e964fef158
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-12-24 22:22:26 +0530

    feat: added datatype examples

--- python2.0/main.py
+++ python2.0/main.py
@@ -2,3 +2,4 @@ print("Hello World")
 
 #type python in terminal to run python interpreter
 #type exit() to exit python interpreter
+# python is dynamicly typed language, they can be reassigned, no data type is required to be specified
