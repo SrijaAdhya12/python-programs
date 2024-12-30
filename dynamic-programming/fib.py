ab22fef48dc1a11751457c0bcedcffd05ba7b654
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-12-30 18:52:48 +0530

    feat: added fibonaci series using memo

--- dynamic-programming/fib.py
+++ dynamic-programming/fib.py
@@ -2,7 +2,40 @@
 
 def fib(n):
     if n <=2:
-         return 1;
+         return 1
     return fib(n-1) + fib(n-2)
 
 print(fib(6))
+
+#using ternary operator
+def fib(n):
+     return 1 if n <= 2 else fib(n-1) + fib(n-2)
+ 
+
+ #fibonacci has a time complexity of O(2^n) and space complexity of O(n)
+
+
+# memorization
+
+def fib(n, memo = {}):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
+
+print(fib(6))
+
+
+
+#fibo using memorization
+def fibo(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    memo[n] = fibo(n-1, memo) + fibo(n-2, memo)
+    return memo[n]
+
+#time complexity of O(n) and space complexity of O(n)
