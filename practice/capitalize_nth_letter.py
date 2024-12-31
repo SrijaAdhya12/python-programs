62f087127b5ba4ec9e1fd2c56785b4fa29743504
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2025-01-01 01:43:55 +0530

    feat: added exception handling

--- practice/capitalize_nth_letter.py
+++ practice/capitalize_nth_letter.py
@@ -1,11 +1,12 @@
 #CAPITALISE NTH LETTER OF A WORD
 
-def capital_letter(sen,index):
-    words = sen.split()
-    new_word = ""
-    for word in words:
-        new_word = word[:index] + word[index].upper() + word[index+1:]
-    return new_word
+def capital_letter(w,i):
+    try:
+        if i < 0:
+            raise IndexError("Index Value must be greater than zero")
+        return w[:i] + w[i].upper() + w[i+1:]
+    except IndexError:
+        return "String index out of range"
 
 
 
