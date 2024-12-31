11aa11cd0c735f467a300b283d9eb132d0233cbd
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-12-31 13:57:42 +0530

    feat: added target sum problem

---
+++ dynamic-programming/target_sum.py
@@ -0,0 +1,45 @@
+# CODE: WAF 'cansum(target,numbers)' that returns a boolean indicating whether or not it is possible to generate the target sum using the numbers in the array. You may use an element of the array as many times as needed. You may assume that all input numbers are nonnegative.
+
+# canSum(7,[5,3,4,7]) -> true
+# canSum(7,[2,4]) -> false
+
+def canSum(targetSum,numbers):
+    if targetSum == 0:
+        return True
+    if targetSum < 0:
+        return False
+    for num in numbers:
+      reminder =  targetSum - num
+      if canSum(reminder,numbers):
+        return True
+    return False
+
+print(canSum(7,[5,3,4,7]))
+print(canSum(7,[2,4]))
+
+# time complexity: O(n^m)
+# space complexity: O(m)
+
+#memoization
+
+def cansum(targetSum, numbers, memo={}):
+   if targetSum in memo:
+      return memo[targetSum]
+   if targetSum == 0:
+      return True
+   if targetSum < 0:
+      return False
+   for num in numbers:
+      reminder = targetSum - num
+      if cansum(reminder, numbers):
+         memo[targetSum] = True
+         return True
+   memo[targetSum] = False
+   return False
+
+print(cansum(7,[5,3,4,7]))
+print(cansum(7,[2,4]))
+
+# time complexity: O(n*m)
+# space complexity: O(m)
+
